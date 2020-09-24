eb5458bd31e07406fb60f0547b0e9987814c83ef
Marian Schwandt <mschwandt@selectline.de>
2020-09-24 08:17:19 +0200

    *removed: Alter Projektplan

--- main.php
+++ main.php
@@ -5,21 +5,3 @@
     <div><?php include_once("templates/Azubis.php"); ?></div>
     <div><?php include_once("templates/Standardplaene.php"); ?></div>
 </div>
-
-
-
-
-<div style="margin-top: 100px; text-align: center;">
-    <button onclick="StartPlanning();">Start planning</button>
-</div>
-<div id="Plan" style="overflow-x: scroll;"></div>
-
-<script>
-    function StartPlanning() {
-
-        jQuery.get("core/CreatePlan.php", function(response){
-            $("#Plan").empty();
-            $("#Plan").append(response);
-        });
-    }
-</script>
