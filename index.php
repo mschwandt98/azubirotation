35422badc127741ecf570ed5d7db33156619d230
Marian Schwandt <mschwandt@selectline.de>
2020-10-14 08:15:51 +0200

    Favicon im Header gesetzt

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ ob_start("minifier");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" />
     <title>Tool für die Azubirotation</title>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
